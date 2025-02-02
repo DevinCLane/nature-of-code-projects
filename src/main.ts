88d46c03d0b45c6db2c691b3638177891ebddc20
Devin Lane <devin.c.lane@gmail.com>
2025-02-02 15:01:59 -0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Nature of Code projects</h1>
+    <p class="read-the-docs">
+      Projects from the Nature of Code book
+    </p>
+  </div>
+`;
