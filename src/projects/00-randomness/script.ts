9f9d79173d30a928b90286631f512fe7f7215d84
Devin Lane <devin.c.lane@gmail.com>
2025-02-06 20:41:49 -0800

    resize canvas. css for project

--- src/projects/00-randomness/script.ts
+++ src/projects/00-randomness/script.ts
@@ -1,50 +1,57 @@
 import p5 from "p5";
 
-function randomWalk(p: p5) {
+class Walker {
+    private x: number;
+    private y: number;
+
+    constructor(private p: p5) {
+        this.x = this.p.width / 2;
+        this.y = this.p.height / 2;
+        this.reset();
+    }
+
+    reset() {
+        this.x = this.p.width / 2;
+        this.y = this.p.height / 2;
+    }
+
+    show() {
+        this.p.stroke(0);
+        this.p.point(this.x, this.y);
+    }
+
+    step() {
+        const choice = this.p.floor(this.p.random(4));
+        if (choice === 0) {
+            this.x++;
+        } else if (choice === 1) {
+            this.x--;
+        } else if (choice === 2) {
+            this.y++;
+        } else {
+            this.y--;
+        }
+    }
+}
+
+function sketch(p: p5) {
     let walker: Walker;
 
-    p.windowResized = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight);
-        walker = new Walker();
+    p.setup = () => {
+        p.createCanvas(500, 500);
+        walker = new Walker(p);
+        p.background(255);
     };
 
-    p.setup = () => {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-        walker = new Walker();
-        p.background(255);
+    p.windowResized = () => {
+        p.resizeCanvas(500, 500);
+        walker.reset();
     };
 
     p.draw = () => {
         walker.step();
         walker.show();
     };
-
-    class Walker {
-        x: number;
-        y: number;
-
-        constructor() {
-            this.x = p.width / 2;
-            this.y = p.height / 2;
-        }
-
-        show() {
-            p.stroke(0);
-            p.point(this.x, this.y);
-        }
-
-        step() {
-            const choice = p.floor(p.random(4));
-            if (choice === 0) {
-                this.x++;
-            } else if (choice === 1) {
-                this.x--;
-            } else if (choice === 2) {
-                this.y++;
-            } else {
-                this.y--;
-            }
-        }
-    }
 }
-new p5(randomWalk);
+
+new p5(sketch);
