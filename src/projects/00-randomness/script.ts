8de14d46457c4a61e952c19370ecd13b3b25412c
Devin Lane <devin.c.lane@gmail.com>
2025-02-07 20:07:06 -0800

    random: add several canvases

--- src/projects/00-randomness/script.ts
+++ src/projects/00-randomness/script.ts
@@ -14,27 +14,21 @@ class Walker {
         this.x = this.p.width / 2;
         this.y = this.p.height / 2;
     }
-
     show() {
         this.p.stroke(0);
         this.p.point(this.x, this.y);
     }
 
     step() {
-        const choice = this.p.floor(this.p.random(4));
-        if (choice === 0) {
-            this.x++;
-        } else if (choice === 1) {
-            this.x--;
-        } else if (choice === 2) {
-            this.y++;
-        } else {
-            this.y--;
-        }
+        let xstep = this.p.floor(this.p.random(3)) - 1;
+        let ystep = this.p.floor(this.p.random(3)) - 1;
+
+        this.x += xstep;
+        this.y += ystep;
     }
 }
 
-function sketch(p: p5) {
+function sketch1(p: p5) {
     let walker: Walker;
 
     p.setup = () => {
@@ -54,4 +48,30 @@ function sketch(p: p5) {
     };
 }
 
-new p5(sketch);
+function sketch2(p: p5) {
+    let walker: Walker;
+
+    p.setup = () => {
+        p.createCanvas(500, 500);
+        walker = new Walker(p);
+        p.background(255);
+    };
+
+    p.windowResized = () => {
+        p.resizeCanvas(500, 500);
+        walker.reset();
+    };
+
+    p.draw = () => {
+        walker.step();
+        walker.show();
+    };
+}
+
+const firstSketch = document.getElementById("sketch1");
+const secondSketch = document.getElementById("sketch2");
+
+if (firstSketch && secondSketch) {
+    new p5(sketch1, firstSketch);
+    new p5(sketch2, secondSketch);
+}
