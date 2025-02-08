932e6734dd295687c377dcef307bfa03aea9d289
Devin Lane <devin.c.lane@gmail.com>
2025-02-08 11:29:55 -0800

    css: homepage styles

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Nature of Code projects</h1>
     <p>
-      Projects from the Nature of Code book, by Devin Lane
+      From the Nature of Code by Daniel Shiffman
+    </p>
+    <p>
+      Projects by Devin Lane
     </p>
     <ul>
     <li><a href="/src/projects/00-randomness/index.html"> 00: Random Walk </a></li>
